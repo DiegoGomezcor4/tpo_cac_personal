83845153d686b1c54d0675a35b25d9481f7ba21a
DiegoGomez <diegocor4@gmail.com>
2023-05-26 23:09:10 -0300

    logo q desaparece

--- js/script.js
+++ js/script.js
@@ -105,14 +105,17 @@ setInterval(cargarImagenAleatoria, 300000);
 
 const menuToggle = document.querySelector('.menu-toggle');
 const menu = document.querySelector('.main-nav');
+const logo = document.querySelector('.logo'); // Agregar esta línea
 
 menuToggle.addEventListener('click', function() {
   if (window.innerWidth <= 768) {
     menu.classList.toggle('active');
     if (menu.classList.contains('active')) {
       menu.style.maxHeight = menu.scrollHeight + 'px';
+      logo.style.display = 'none'; // Ocultar el logo cuando el menú está activo
     } else {
       menu.style.maxHeight = 0;
+      logo.style.display = 'block'; // Mostrar el logo cuando el menú está inactivo
     }
   }
 });
@@ -121,5 +124,6 @@ window.addEventListener('resize', function() {
   if (window.innerWidth > 768) {
     menu.classList.remove('active');
     menu.style.maxHeight = 'none';
+    logo.style.display = 'block'; // Asegurarse de mostrar el logo cuando el menú no está activo en pantallas más grandes
   }
 });
